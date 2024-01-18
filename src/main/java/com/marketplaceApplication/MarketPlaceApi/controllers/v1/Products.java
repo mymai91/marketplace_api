d1e34ebd7e28502267f120007e93b98b10084647
my.mai <my.mai@we-are-mea.com>
2024-01-19 09:07:13 +1300

    product controller

---
+++ src/main/java/com/marketplaceApplication/MarketPlaceApi/controllers/v1/Products.java
@@ -0,0 +1,9 @@
+package com.marketplaceApplication.MarketPlaceApi.controllers.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class Products {
+}
